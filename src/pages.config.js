0e28e8e4554279c037f7c5e001b99943a64fdb8d
Davinder Singh <davinder.aoulakh@gmail.com>
2026-01-17 07:32:44 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -2,13 +2,13 @@ import Admin from './pages/Admin';
 import Home from './pages/Home';
 import LiveBetting from './pages/LiveBetting';
 import MyBets from './pages/MyBets';
-import Onboarding from './pages/Onboarding';
 import Privacy from './pages/Privacy';
 import Profile from './pages/Profile';
 import ResponsibleGambling from './pages/ResponsibleGambling';
 import Sports from './pages/Sports';
 import Terms from './pages/Terms';
 import Wallet from './pages/Wallet';
+import Onboarding from './pages/Onboarding';
 import __Layout from './Layout.jsx';
 
 
@@ -17,13 +17,13 @@ export const PAGES = {
     "Home": Home,
     "LiveBetting": LiveBetting,
     "MyBets": MyBets,
-    "Onboarding": Onboarding,
     "Privacy": Privacy,
     "Profile": Profile,
     "ResponsibleGambling": ResponsibleGambling,
     "Sports": Sports,
     "Terms": Terms,
     "Wallet": Wallet,
+    "Onboarding": Onboarding,
 }
 
 export const pagesConfig = {
